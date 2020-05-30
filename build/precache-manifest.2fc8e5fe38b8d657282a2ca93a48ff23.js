592b0ed42339a0e9d41d9fd609d5b723152a0d71
telly <telly@Dev3.local>
2020-05-30 06:23:11 -0400

    build folder

---
+++ build/precache-manifest.2fc8e5fe38b8d657282a2ca93a48ff23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63289a433987f66c61c2da513ac7d982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07ddc5308f17963dd701",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "caec4599c57e64f1d5c5",
+    "url": "/static/js/2.69a7e292.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.69a7e292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ddc5308f17963dd701",
+    "url": "/static/js/main.baa87338.chunk.js"
+  },
+  {
+    "revision": "e00696394c974a4b406a",
+    "url": "/static/js/runtime-main.608bdef6.js"
+  }
+]);
